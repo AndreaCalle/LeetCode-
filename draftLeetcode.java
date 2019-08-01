d240a1e3b82d98b2dffb6963fe1d126c8285c8e3
AndreaCalle <43764730+AndreaCalle@users.noreply.github.com>
2019-08-01 15:43:38 -0400

    Update draftLeetcode.java

--- draftLeetcode.java
+++ draftLeetcode.java
@@ -17,7 +17,67 @@
         }
         return 1 ;
     }
+Question #1 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] temp = new int[2]; 
+        if(nums.length == 0 ) return temp ; 
+        for(int i = 0 ; i < nums.length ; i ++){
+            for(int j = i+1; j< nums.length ; j++){
+                if((nums[i]+nums[j]) == target ){
+                     temp[0] = i; 
+                     temp[1] = j; 
+                }
+            }
+        }
+       return temp ; 
+    }
+}
+Question #2 
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode result = head; 
+        int t = 0; 
+       int t2 = 0;
+        int temp = 0 ; 
+        while( l1 != null || l2 != null){            
+            if(l1 != null) {
+                   t = l1.val; 
+                  l1 =  l1.next;
+            }
+            else t = 0 ; 
+ 
+            if(l2 != null) {
+                  t2 = l2.val;
+                  l2 =  l2.next;
+              }
+            else t2 = 0 ; 
 
+            int r = t + t2 ;
+           if(temp != 0 ){
+                r += temp ; 
+                temp = 0 ; 
+            }
+            if(r > 9) {
+                temp++ ; 
+                r = r%10 ;  
+            }
+            
+            result.next = new ListNode(r) ; 
+            result = result.next;
+
+
+        }
+        if(temp != 0 )
+        {
+            result.next = new ListNode(temp) ; 
+            result = result.next;
+        }
+            
+        return head.next; 
+    }
+}
 Question #4 Hacker Leet 
     //import com.google.common.collect.ObjectArrays;
 import java.util.Arrays;
