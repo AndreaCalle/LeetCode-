547b5c960f2a6ad8f970442aa5296f59c68d6754
AndreaCalle <43764730+AndreaCalle@users.noreply.github.com>
2020-09-28 16:32:13 -0400

    Update LongestSubstring.java

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -1,6 +1,7 @@
 /*
 Longest Substring Without Repeating Characters
 Given a string s, find the length of the longest substring without repeating characters.
+COMPELETED
 */
 class Solution {
     public int lengthOfLongestSubstring(String s) {
