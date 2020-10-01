f2e3507a158c16b12daa614aeff41660f70d5f76
AndreaCalle <43764730+AndreaCalle@users.noreply.github.com>
2020-10-01 17:28:00 -0400

    Update ContainerWithMostWater.java

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -3,7 +3,7 @@ Container With Most Water
 Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
 n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
 Find two lines, which together with x-axis forms a container, such that the container contains the most water.
-COMPELETED
+COMPLETED
 */
 class Solution {
     public int maxArea(int[] height) {
